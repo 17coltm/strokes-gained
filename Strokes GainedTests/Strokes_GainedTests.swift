2c53e467f4828a537fc30f0146d15371849d7445
coltm <Colt@Colts-MacBook-Air.local>
2015-09-10 20:23:35 -0700

    Initial Commit

---
+++ Strokes GainedTests/Strokes_GainedTests.swift
@@ -0,0 +1,36 @@
+//
+//  Strokes_GainedTests.swift
+//  Strokes GainedTests
+//
+//  Created by Colt McNealy on 9/10/15.
+//  Copyright (c) 2015 coltmcnealy. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Strokes_GainedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
